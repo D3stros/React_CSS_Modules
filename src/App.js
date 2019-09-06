2a9a0d2b4899c8791a6ff38ac32791c31110aa2a
Destros <patrick.fallmann@hotmail.com>
2019-09-06 21:16:20 +0200

    Using css modules

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import UserInput from "./UserInput";
+import UserOutput from "./UserOutput";
+  // CSS modules
+import classes from "./App.module.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      {name: "Daniel"}
+    ]
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({persons: [
+      {name: event.target.value},
+    ]})
+  }
+  render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    }
+    return (
+      // CSS modules
+      <div className={classes.App}>
+        <UserInput changed={this.nameChangedHandler} name = {this.state.persons[0].namecd}/>
+        <UserOutput name = {this.state.persons[0].name} />
+        <UserOutput name = {this.state.persons[0].name} />
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/UserOutput.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserOutput = (props) => {
+  return (
+    <div className="UserOutput">
+    <p>Username is: {props.name}</p>
+    <p>Hi how are you</p>
+    </div>
+  )
+};
+
+export default UserOutput;
